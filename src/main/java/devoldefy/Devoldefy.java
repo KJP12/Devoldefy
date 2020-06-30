ad16eb40677b2d80a1dac315ae59097e2e4c05cb
KJP12 <kjp12.k.joy.programer12@gmail.com>
2020-06-30 02:52:49 -0700

    Doesn't remap too well without something to reference, but hey, it's a bit more updated now.

--- src/main/java/devoldefy/Devoldefy.java
+++ src/main/java/devoldefy/Devoldefy.java
@@ -7,7 +7,10 @@ import org.cadixdev.mercury.Mercury;
 import org.cadixdev.mercury.remapper.MercuryRemapper;
 
 import java.io.*;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
@@ -15,70 +18,73 @@ import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 public class Devoldefy {
-    private static final String CSV = "http://export.mcpbot.bspk.rs/mcp_{csv_type}_nodoc/{csv_build}-{mc_version}/mcp_{csv_type}_nodoc-{csv_build}-{mc_version}.zip";
-    private static final String SRG = "https://raw.githubusercontent.com/MinecraftForge/MCPConfig/master/versions/{mc_version}/joined.tsrg";
-    private static final String YARN = "http://maven.modmuss50.me/net/fabricmc/yarn/{target_minecraft_version}+build.{yarn_build}/yarn-{target_minecraft_version}+build.{yarn_build}.jar";
-    private static final String FORGE_JAR = "{home_dir}/.gradle/caches/minecraft/net/minecraftforge/forge/{mc_version}-{forge_version}/{csv_type}/{csv_build}/forgeSrc-{mc_version}-{forge_version}.jar";
+    static final FileSystem fileSystem = FileSystems.getDefault();
+    static final Path workDirctory = fileSystem.getPath(System.getProperty("user.dir"));
 
     public static void main(String[] args) throws Exception {
-        File files = new File("files");
-
-        String sourceMinecraftVersion = ask("Source Minecraft version", "1.13.2");
-        String sourceMappingsVersion = ask("Source mappings version", "1.13.2");
-        String sourceForgeVersion = ask("Source forge version", "14.23.4.2703");
-        String sourceMappingsType = ask("Source mappings type", "snapshot");
-        String sourceMappingsBuild = ask("Source mappings build", "20190424");
-
-        String targetMinecraftVersion = ask("Target Minecraft version", "1.14");
-        String targetYarnBuild = ask("Target Yarn build", "2");
-
-        String sourceRoot = ask("Path to source root", "./src/main/java");
-        String targetRoot = ask("Path to target root", "./updated_src/main/java");
-
-        devoldefy(files, sourceMinecraftVersion, sourceMappingsVersion, sourceForgeVersion, sourceMappingsType, sourceMappingsBuild, targetMinecraftVersion, targetYarnBuild, sourceRoot, targetRoot);
+        Properties properties = FileHelper.loadProperties();
+        if(properties == null) {
+            System.out.println("Please fill out ./config.properties to fit your needs.");
+            System.exit(0);
+            throw new AssertionError("JVM should've exited?!");
+        }
+        String forgeJar = properties.getProperty("forge.jar");
+        devoldefy(fileSystem.getPath("devoldefy-tmp"),
+                fileSystem.getPath(properties.getProperty("source.root")),
+                fileSystem.getPath(properties.getProperty("target.root")),
+                replaceProperties(properties.getProperty("mappings.mcp.csv"), properties),
+                replaceProperties(properties.getProperty("mappings.mcp.srg"), properties),
+                replaceProperties(properties.getProperty("mappings.yarn"), properties),
+                forgeJar == null ? null : fileSystem.getPath(replaceProperties(forgeJar.replace("{home_dir}", workDirctory.toString()), properties)),
+                properties.getProperty("source.version"));
     }
 
-    public static void devoldefy(File files, String sourceMinecraftVersion, String sourceMappingsVersion, String sourceForgeVersion,
-                                 String sourceMappingsType, String sourceMappingsBuild, String targetMinecraftVersion, String targetYarnBuild,
-                                 String sourceRoot, String targetRoot) throws Exception {
-        String csvUrl = CSV.replace("{mc_version}", sourceMappingsVersion).replace("{csv_type}", sourceMappingsType).replace("{csv_build}", sourceMappingsBuild);
-        String srgUrl = SRG.replace("{mc_version}", targetMinecraftVersion);
-        String yarnUrl = YARN.replace("{target_minecraft_version}", targetMinecraftVersion).replace("{yarn_build}", targetYarnBuild);
-        String forgeLocation = FORGE_JAR.replace("{home_dir}", System.getProperty("user.home")).replace("{mc_version}", sourceMinecraftVersion).replace("{forge_version}", sourceForgeVersion).replace("{csv_type}", sourceMappingsType).replace("{csv_build}", sourceMappingsBuild);
+    public static void devoldefy(Path files,
+                                 Path sourceRoot, Path targetRoot,
+                                 String csvUrl, String srgUrl, String yarnUrl, Path forgeLocation,
+                                 String javaVersion) throws Exception {
+        Files.createDirectories(files);
+        Mappings srg;
+        try(FileSystem fileSystem = FileSystems.newFileSystem(URI.create("jar:" + FileHelper.download(csvUrl, files).toUri().toString()), Collections.emptyMap())) {
+            srg = Readers.tsrg(
+                    new Scanner(FileHelper.download(srgUrl, files)),
+                    Readers.csv(new Scanner(fileSystem.getPath("fields.csv"))),
+                    Readers.csv(new Scanner(fileSystem.getPath("methods.csv")))
+            );
+        }
 
-        Mappings srg = Readers.tsrg(
-            new Scanner(FileHelper.download(srgUrl, files)),
-            Readers.csv(new Scanner(FileHelper.extract(FileHelper.download(csvUrl, files), "fields.csv", files))),
-            Readers.csv(new Scanner(FileHelper.extract(FileHelper.download(csvUrl, files), "methods.csv", files)))
-        );
-
-        Mappings yarn = Readers.tiny(
-            new Scanner(FileHelper.extract(FileHelper.download(yarnUrl, files), "mappings/mappings.tiny", files)),
-            "official",
-            "named"
-        );
+        Mappings yarn;
+        try(FileSystem fileSystem = FileSystems.newFileSystem(URI.create("jar:" + FileHelper.download(yarnUrl, files).toUri().toString()), Collections.emptyMap())) {
+            yarn = Readers.tiny(
+                    new Scanner(fileSystem.getPath("mappings/mappings.tiny")),
+                    "official",
+                    "named"
+            );
+        }
 
         Mappings mappings = srg.invert().chain(yarn);
-
-        File sourceDir = new File(sourceRoot);
-        File targetDir = new File(targetRoot);
-        Files.walk(targetDir.toPath()).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
-        targetDir.mkdirs();
+        if(Files.exists(targetRoot)) {
+            Files.walk(targetRoot).sorted(Comparator.reverseOrder()).forEach(it -> {
+                try {
+                    Files.delete(it);
+                } catch (IOException ioe) {
+                    throw new IOError(ioe);
+                }
+            });
+        } else {
+            Files.createDirectories(targetRoot);
+        }
 
         List<Path> classpath = new ArrayList<>();
-        File forgeJar = new File(forgeLocation);
-        if (!forgeJar.exists()) {
-            throw new IllegalStateException("Forge jar not found at " + forgeJar.getCanonicalPath());
+        if(forgeLocation != null) {
+            if(Files.notExists(forgeLocation))
+                throw new IllegalStateException("Forge jar not found at " + forgeLocation.toAbsolutePath());
+            classpath.add(forgeLocation);
+        } else {
+            System.err.println("Warning: A remapped Forge/Minecraft isn't defined.\nYou may want to define either one of them to ensure that remapping goes as intended.");
         }
-        classpath.add(forgeJar.toPath());
 
-        remap(sourceDir.toPath(), targetDir.toPath(), classpath, mappings);
-    }
-
-    private static String ask(String message, String fallback) {
-        System.out.print(message + (fallback == null ? "" : " (or blank for " + fallback + ")") + ": ");
-        String result = new Scanner(System.in).nextLine().trim();
-        return result.isEmpty() ? fallback : result;
+        remap(sourceRoot, targetRoot, classpath, mappings, javaVersion);
     }
 
     static String hash(String s) {
@@ -96,8 +102,12 @@ public class Devoldefy {
         }
     }
 
-    private static void remap(Path source, Path target, List<Path> classpath, Mappings mappings) throws Exception {
+    private static void remap(Path source, Path target, List<Path> classpath, Mappings mappings, String javaVersion) throws Exception {
         Mercury mercury = new Mercury();
+        // Used to allow missing Forge/Minecraft.
+        // This may unexpectedly break stuff.
+        mercury.setGracefulClasspathChecks(true);
+        mercury.setSourceCompatibility(javaVersion);
         mercury.getClassPath().addAll(classpath);
 
         MappingSet mappingSet = new MappingSetImpl(new MappingSetModelFactoryImpl());
@@ -191,4 +201,17 @@ public class Devoldefy {
         }
     }
 
+    static String replaceProperties(String string, Properties properties) {
+        StringBuilder builder = new StringBuilder(string);
+        int l = 0;
+        while((l = builder.indexOf("${", l)) > 0) {
+            int f = builder.indexOf("}", l);
+            String prop = builder.substring(l + 2, f);
+            String str = properties.getProperty(prop);
+            if(str == null) throw new NullPointerException(prop);
+            builder.replace(l, f + 1, str);
+            l += str.length();
+        }
+        return builder.toString();
+    }
 }
--- src/main/java/devoldefy/FileHelper.java
+++ src/main/java/devoldefy/FileHelper.java
@@ -1,52 +1,63 @@
 package devoldefy;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.Properties;
 
 public class FileHelper {
-	static File download(String url, File directory) throws IOException {
-		directory.mkdirs();
-		File file = new File(directory, Devoldefy.hash(url));
+	static Path download(String url, Path directory) throws IOException {
+		Path file = directory.resolve(Devoldefy.hash(url));
 
-		if (!file.exists()) {
+		if (Files.notExists(file)) {
 			try (InputStream in = new URL(url).openStream()) {
-				Files.copy(in, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(in, file, StandardCopyOption.REPLACE_EXISTING);
 			}
 		}
 
 		return file;
 	}
 
-	static File extract(File zip, String path, File directory) throws IOException {
-		directory.mkdirs();
-		File file = new File(directory, Devoldefy.hash(zip.getName() + path));
+	/**
+	 * Loads properties from user.dir
+	 * */
+	static Properties loadProperties() throws IOException {
+		Properties properties = new Properties();
+		Path path = Devoldefy.workDirctory.resolve("config.properties");
+		if(Files.notExists(path)) {
+			Files.createFile(path);
+			properties.put("mappings.mcp.csv", "http://export.mcpbot.bspk.rs/mcp_${mcp.type}_nodoc/${mcp.build}-${mcp.version}/mcp_${mcp.type}_nodoc-${mcp.build}-${mcp.version}.zip");
+			properties.put("mappings.mcp.srg", "https://raw.githubusercontent.com/MinecraftForge/MCPConfig/master/versions/${source.version.type}/${mcp.config}/joined.tsrg");
+			properties.put("mappings.yarn", "http://maven.modmuss50.me/net/fabricmc/yarn/${target.version}+build.${yarn.build}/yarn-${target.version}+build.${yarn.build}.jar");
+			properties.put("forge.jar", "{home_dir}/.gradle/caches/minecraft/net/minecraftforge/forge/${source.version}-${forge.version}/${mcp.type}/${mcp.build}/forgeSrc-${source.version}-${forge.version}.jar");
 
-		try (ZipFile zipFile = new ZipFile(zip)) {
-			InputStream is = null;
+			properties.put("forge.version", "See https://files.minecraftforge.net or your build file");
+			properties.put("source.java.version", "1.8");
+			properties.put("source.root", "./src/main/java");
+			properties.put("source.version", "1.12");
+			properties.put("source.version.type", "release");
+			properties.put("mcp.config", "1.12.2");
+			properties.put("mcp.version", "1.12");
+			properties.put("mcp.type", "stable");
+			properties.put("mcp.build", "39");
 
-			Enumeration<? extends ZipEntry> entries = zipFile.entries();
-			while (entries.hasMoreElements()) {
-				ZipEntry zipEntry = entries.nextElement();
-				if (zipEntry.getName().equals(path)) {
-					is = zipFile.getInputStream(zipEntry);
-					break;
-				}
+			properties.put("target.root", "./remappedSrc");
+			properties.put("target.version", "1.14.4");
+			properties.put("yarn.build", "8");
+
+			try(OutputStream stream = Files.newOutputStream(path)) {
+				properties.store(stream, "Please modify the variables to what you need.");
 			}
-
-			if (is == null) {
-				return null;
+			return null;
+		} else {
+			try(InputStream stream = Files.newInputStream(path)) {
+				properties.load(stream);
 			}
-
-			Files.copy(is, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			return properties;
 		}
-
-		return file;
 	}
 }
